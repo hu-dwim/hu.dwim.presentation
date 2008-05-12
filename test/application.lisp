6d0f0ca4367fea4df35c34f16e7ed6b3b726e560
Attila Lendvai <attila@lendvai.name>
2008-05-12 09:35:13 +0000

    tests

--- test/application.lisp
+++ test/application.lisp
@@ -5,13 +5,42 @@
 (def entry-point (*test-application* :path "params") ((number "0" number?) ((the-answer "theanswer") "not supplied" the-answer?))
   (make-functional-html-response ()
     (with-html-document-body (:title "foo")
-      <p "number: " ,(princ-to-string number)>
-      <p "the-answer: " ,(princ-to-string the-answer)>
-      <a (:href ,(concatenate-string (path-prefix-of *test-application*)
-                                     (if (or number? the-answer?)
-                                         "params"
-                                         "params?theanswer=yes&number=42")))
-        "try this">)))
+      <p "Parameters:"
+        <a (:href ,(concatenate-string (path-prefix-of *test-application*)
+                                       (if (or number? the-answer?)
+                                           "params"
+                                           "params?theanswer=yes&number=42")))
+          "try this">>
+      <table
+        <thead <tr <td "Parameter name"> <td "Parameter value">>>
+        ,@(do-parameters (name value)
+            <tr
+              <td ,name>
+              <td ,(etypecase value
+                     (string value)
+                     (list (princ-to-string value))
+                     (rfc2388-binary:mime-part
+                      (bind ((mime-part value))
+                        <p "Mime part headers:">
+                        <table
+                        <thead <tr <td "Header"> <td "Value">>>
+                        ,@(iter (for header :in (rfc2388-binary:headers mime-part))
+                                (collect <tr
+                                           <td ,(or (rfc2388-binary:header-name header) "")
+                                           <td ,(or (rfc2388-binary:header-value header) "")>>>))>
+                        <p "Mime part content: " ,(princ-to-string (rfc2388-binary:content mime-part))>)))>>)>
+      <hr>
+      <form (:method "post")
+        <input (:name "input1"             :value ,(or (parameter-value "input1") "1"))>
+        <input (:name "input2-with-áccent" :value ,(or (parameter-value "input2-with-áccent") "\"Ááőűúéö\""))>
+        <input (:type "submit")>>
+      <form (:method "post" :enctype "multipart/form-data")
+        <input (:name "file-input1" :type "file")>
+        <input (:name "file-input2" :value ,(or (parameter-value "file-input2") "file2"))>
+        <input (:type "submit")>>
+      <hr>
+      <p "The parsed request: ">
+      (render-request *request*))))
 
 (def special-variable *echo-application* (make-application :path-prefix "/echo/"))
 
@@ -21,11 +50,16 @@
 (def special-variable *session-application* (make-application :path-prefix "/session/"))
 
 (def entry-point (*session-application* :path "") ()
-  (bind ((session *session*))
+  (bind ((session *session*)
+         (application *application*))
     (make-functional-response ((+header/content-type+ +html-content-type+))
       (emit-into-html-stream (network-stream-of *request*)
         (with-html-document-body ()
-          <p "session: " ,(princ-to-string session)>
+          <p "session: " ,(princ-to-string session)
+             ,@(unless session
+                 (list <a (:href ,(concatenate-string (path-prefix-of application)
+                                                      "new/"))
+                         "new session">))>
           <hr>
           (render-request *request*))))))
 
@@ -37,6 +71,9 @@
             ;; app -> session locking order...
             (with-lock-held-on-application *application*
               (make-new-session *application*)))))
+
+(ensure-entry-point *session-application*
+                    (make-file-serving-broker "/session/wui/" (project-relative-pathname "")))
 
 (def function start-server-with-test-applications (&key (maximum-worker-count 16) (log-level +dribble+))
   (with-logger-level wui log-level
