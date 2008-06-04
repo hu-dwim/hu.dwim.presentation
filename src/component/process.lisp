2e37f61bd36601b1c986fe25cb789461b39dbf95
Levente Mészáros <levente.meszaros@gmail.com>
2008-06-04 16:43:52 +0000

    Revive process.

--- src/component/process.lisp
+++ src/component/process.lisp
@@ -7,9 +7,10 @@
 ;;;;;;
 ;;; Process
 
+;; TODO: try to kill this variable!
 (def special-variable *standard-process-component*)
 
-(def component standard-process-component (content-component)
+(def component standard-process-component (content-component user-message-collector-component-mixin)
   ((form)
    (closure/cc nil)
    (answer-continuation nil)))
@@ -25,7 +26,9 @@
                 (funcall closure/cc)))))
     (unless (and content answer-continuation)
       (setf content (make-instance 'label-component :component-value "Process finished")))
-    (call-next-method)))
+    <div
+     ,(render-user-messages -self-)
+     ,(render content)>))
 
 (def (macro e) make-standard-process-component (&rest args &key form &allow-other-keys)
   (remove-from-plistf args :form)
@@ -45,6 +48,9 @@
       (setf (answer-continuation-of *standard-process-component*)
             (kall (answer-continuation-of *standard-process-component*) value)))))
 
+;;;;;;
+;;; Answer command
+
 (def component answer-command-component (command-component)
   ((icon (make-icon-component 'answer :label "Answer"))
    (action)
@@ -52,7 +58,10 @@
 
 (def constructor answer-command-component ()
   (with-slots (icon action value) -self-
-    (setf action (make-action (answer -self- value)))))
+    (setf action (make-action (answer -self- (force value))))))
+
+(def (macro e) answer-command (icon &body forms)
+  `(make-instance 'answer-command-component :icon ,icon :value (delay ,@forms)))
 
 (def method cl-quasi-quote::collect-slots-for-syntax-node-emitting-form ((node answer-command-component))
   (remove 'action (call-next-method) :key #'slot-definition-name))
@@ -78,7 +87,7 @@
       (setf content (make-instance 'label-component :component-value "Process finished")))
     (call-next-method)))
 
-(def macro dwim-meta-model:show-to-subject (component &optional subject)
+(def macro dmm:show-to-subject (component &optional subject)
   "Shows a user interface component to the given subject."
   (once-only (subject)
     `(dmm::show-and-wait ,component
@@ -89,7 +98,7 @@
                                         :subject (or ,subject
                                                      (dmm::current-effective-subject))))))
 
-(def macro dwim-meta-model:show-to-subject-expression (component &optional expression)
+(def macro dmm:show-to-subject-expression (component &optional expression)
   "Shows a user interface component to any one of the subjects matching to the given expression"
   `(dmm::show-and-wait ,component
                        (and (dmm::has-authenticated-session)
@@ -99,14 +108,15 @@
                        (or (dmm::select-wait-for-expression :wait-for-subject #t :expression ',expression)
                            (dmm::make-wait-for-expression :wait-for-subject #t :expression ',expression))))
 
-(def macro dwim-meta-model:show-and-wait (component &optional (condition t) (wait-reason nil))
+;; TODO: defun/cc?
+(def macro dmm:show-and-wait (component &optional (condition t) (wait-reason nil))
   `(progn
      (dmm::persistent-process-wait dmm::*process* ,wait-reason)
-     (if ,condition
-         (let (self)
-           (setf self ,component)
-           (call self))
-         (let/cc k k))
+     (call (if ,condition
+               ,component
+               (progn
+                 (add-user-information *standard-process-component* #"process.message.waiting-for-other-subject")
+                 (empty))))
      (assert (eq (dmm::element-name-of (dmm::process-state-of dmm::*process*)) 'dmm::running))))
 
 (def method answer ((component persistent-process-component) value)
@@ -114,5 +124,32 @@
     (rdbms::with-transaction
       (prc::revive-instance (process-of component))
       (dmm::with-new-persistent-process-context (:renderer component :process (process-of component))
+        (dmm::process-event (process-of component) 'dmm::process-state 'dmm::continue)
         (setf (answer-continuation-of *standard-process-component*)
-              (nth-value 1 (dmm::continue-persistent-process-in-context)))))))
+              (kall (answer-continuation-of *standard-process-component*) value))))))
+
+(defresources hu
+  (process.message.waiting-for-other-subject "A folyamat jelenleg máshoz tartozik.")
+  (process.message.waiting "A folyamat jelenleg várakozik.")
+  (process.message.report-process-state (process)
+    (ecase (dmm::element-name-of (dmm::process-state-of process))
+      ;; a process in 'running state may not reach this point
+      (dmm::finished    "Folyamat normálisan befejeződött")
+      (dmm::failed      "Folyamat hibára futott")
+      (dmm::broken      "Folyamat technikai hiba miatt megállítva")
+      (dmm::cancelled   "Folyamat felhasználó által leállítva")
+      (dmm::in-progress "Folyamat folyamatban")
+      (dmm::paused      "Folyamat félbeszakítva"))))
+
+(defresources en
+  (process.message.waiting-for-other-subject "Process is waiting for other subject.")
+  (process.message.waiting "Process is currently waiting.")
+  (process.message.report-process-state (process)
+    (ecase (dmm::element-name-of (dmm::process-state-of process))
+      ;; a process in 'running state may not reach this point
+      (dmm::finished    "Process finished normally")
+      (dmm::failed      "Process failed")
+      (dmm::broken      "Process was stopped due to technical failures")
+      (dmm::cancelled   "Process has been cancelled")
+      (dmm::in-progress "Process is in progress")
+      (dmm::paused      "Process is paused"))))
