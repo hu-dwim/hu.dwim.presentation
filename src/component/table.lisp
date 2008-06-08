6703c158c40b51c8db81c3254312d99bb59c95a5
Attila Lendvai <attila@lendvai.name>
2008-06-08 21:02:10 +0000

    add visible support for table columns

--- src/component/table.lisp
+++ src/component/table.lisp
@@ -17,30 +17,34 @@
     (setf page-navigation-bar (make-instance 'page-navigation-bar-component :position 0 :page-count 10))))
 
 (def render table-component ()
-  (with-slots (columns rows page-navigation-bar) -self-
-    (setf (total-count-of page-navigation-bar) (length rows))
-    <div
-      <table
-        <thead
-          <tr ,@(mapcar #'render columns)>>
-      <tbody ,@(mapcar #'render
-                       (subseq rows
-                               (position-of page-navigation-bar)
-                               (min (length rows)
-                                    (+ (position-of page-navigation-bar)
-                                       (page-count-of page-navigation-bar)))))>>
-      ,(if (< (page-count-of page-navigation-bar) (total-count-of page-navigation-bar))
-           (render page-navigation-bar)
-           +void+)>))
+  (bind ((table -self-))
+    (with-slots (columns rows page-navigation-bar) table
+      (setf (total-count-of page-navigation-bar) (length rows))
+      <div
+       <table
+           <thead
+            <tr ,@(mapcar #'render columns)>>
+         <tbody
+          ,@(bind ((visible-rows (subseq rows
+                                         (position-of page-navigation-bar)
+                                         (min (length rows)
+                                              (+ (position-of page-navigation-bar)
+                                                 (page-count-of page-navigation-bar))))))
+              (iter (for row :in-sequence visible-rows)
+                    (collect (render-table-row table row))))>>
+       ,(if (< (page-count-of page-navigation-bar) (total-count-of page-navigation-bar))
+            (render page-navigation-bar)
+            +void+)>)))
 
 (def component column-component (content-component)
-  ())
+  ((visible #t :type boolean)))
 
 (def macro column (content)
   `(make-instance 'column-component :content ,content))
 
 (def render column-component ()
-  <th ,(call-next-method)>)
+  (when (visible-p -self-)
+    <th ,(call-next-method)>))
 
 (def component row-component ()
   ((cells nil :type components)))
@@ -50,12 +54,24 @@
       "even-row"
       "odd-row"))
 
+(def layered-function render-table-row (table row)
+  (:method ((table table-component) (row row-component))
+    (assert (eq (parent-component-of row) table))
+    <tr (:class ,(odd/even-class row (rows-of table)))
+      ,@(iter (for cell :in-sequence (cells-of row))
+              (for column :in-sequence (columns-of table))
+              (when (force (visible-p column))
+                (collect (render-table-cell table row column cell))))>))
+
+#+nil ; TODO delme?
 (def render row-component ()
-  <tr (:class ,(odd/even-class -self- (rows-of (parent-component-of -self-))))
-    ,@(mapcar #'render (cells-of -self-))>)
+  (render-table-row (parent-component-of -self-) -self-))
 
 (def component entire-row-component (content-component)
   ())
+
+(def layered-method render-table-row ((table table-component) (row entire-row-component))
+  (render row))
 
 (def function render-entire-row (table body-thunk)
   <tr
@@ -68,5 +84,10 @@
 (def component cell-component (content-component)
   ())
 
+(def layered-function render-table-cell (table row column cell)
+  (:method (table row column cell)
+    <td ,(render cell)>))
+
+#+nil ; TODO delme?
 (def render cell-component ()
   <td ,(call-next-method)>)
