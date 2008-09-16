e8f9314e692e08634831d924a711e93baa65ec3a
Levente Mészáros <levente.meszaros@gmail.com>
2008-09-16 09:21:32 +0000

    Added tab-page component.

--- src/component/tab-container.lisp
+++ src/component/tab-container.lisp
@@ -8,34 +8,28 @@
 ;;; Tab container component
 
 (def component tab-container-component (content-component)
-  ((icons nil :type components)
-   (pages nil :type components)
+  ((pages nil :type components)
    (command-bar nil :type component)))
 
-(def constructor tab-container-component ()
-  (with-slots (icons pages command-bar) -self-
-    (setf command-bar
-          (make-instance 'command-bar-component
-                         :commands (mapcar (lambda (icon page)
-                                             (make-switch-to-tab-page-command -self- icon page))
-                                           icons pages)))))
-
-(def (macro e) tab-container (&body icon-page-pairs)
-  `(make-instance 'tab-container-component
-                  :icons (list ,@(mapcar #'first icon-page-pairs))
-                  :pages (list ,@(mapcar #'second icon-page-pairs))))
+(def (macro e) tab-container (&body pages)
+  `(make-instance 'tab-container-component :pages (list ,@pages)))
 
 (def render tab-container-component ()
   (with-slots (content command-bar) -self-
     (render-vertical-list (list command-bar content))))
 
 (def method refresh-component ((self tab-container-component))
-  (with-slots (pages content) self
+  (with-slots (pages command-bar content) self
+    (setf command-bar
+          (make-instance 'command-bar-component
+                         :commands (mapcar (lambda (page)
+                                             (make-switch-to-tab-page-command self page))
+                                           pages)))
     (unless content
       (setf content (first pages)))))
 
-(def function make-switch-to-tab-page-command (tab-container icon tab-page)
-  (make-replace-command (delay (content-of tab-container)) tab-page :icon icon))
+(def function make-switch-to-tab-page-command (tab-container tab-page)
+  (make-replace-command (delay (content-of tab-container)) tab-page :icon (icon-of tab-page)))
 
 (def icon swith-to-page "static/wui/icons/20x20/eye.png")
 (defresources hu
@@ -44,3 +38,12 @@
 (defresources en
   (icon-label.swith-to-page "Page")
   (icon-tooltip.swith-to-page "Switch to page"))
+
+;;;;;;
+;;; Tab page
+
+(def component tab-page-component (content-component)
+  ((icon nil :type component)))
+
+(def (macro e) tab-page (icon &body forms)
+  `(make-instance 'tab-page-component :icon ,icon :content (progn ,@forms)))
