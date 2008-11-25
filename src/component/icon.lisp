5b49089e1902683316fd52b3a921b189f9c33890
Levente Mészáros <levente.meszaros@gmail.com>
2008-11-25 12:40:37 +0000

    Allow strings as icon names.

--- src/component/icon.lisp
+++ src/component/icon.lisp
@@ -58,7 +58,10 @@
             (apply #'make-instance 'icon-component :name name args)
             (error "The icon ~A cannot be found and no arguments were specified" name)))))
 
-(def special-variable *icons* (make-hash-table))
+;; TODO: KLUDGE: do we really need an equal hash table here?
+;; TODO: this was added because there are lookups with keys concatenated and there's no way currently
+;; TODO: to find out the correct package, see member-component
+(def special-variable *icons* (make-hash-table :test #'equal))
 
 (def (function e) find-icon (name &key (otherwise `(:error "The icon ~A cannot be found" ,name)))
   (prog1-bind icon (gethash name *icons*)
