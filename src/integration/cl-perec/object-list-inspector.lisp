f0e19c6f0b3d9e1ffa55e8ecba875dc397d39b62
Levente Mészáros <levente.meszaros@gmail.com>
2008-11-25 15:47:31 +0000

    Even more authorization.

--- src/integration/cl-perec/object-list-inspector.lisp
+++ src/integration/cl-perec/object-list-inspector.lisp
@@ -11,8 +11,15 @@
   (remove-if #'prc:persistent-object-internal-slot-p (call-next-method)))
 
 (def layered-method collect-standard-object-list-table-inspector-slots ((component standard-object-list-table-inspector) (class dmm::entity) (instance prc::persistent-object))
-  (filter-if #'dmm::primary-p (call-next-method)))
+  (filter-if (lambda (slot)
+               (and (dmm::primary-p slot)
+                    (dmm::authorize-operation 'dmm::read-entity-property-operation :-entity- class :-property- slot)))
+             (call-next-method)))
 
 (def layered-method make-expand-command ((component standard-object-row-inspector) (class prc::persistent-class) (instance prc::persistent-object))
   (when (dmm::authorize-operation 'expand-instance-operation :-entity- class)
+    (call-next-method)))
+
+(def layered-method make-editing-commands ((component abstract-standard-object-list-component) (class prc::persistent-class) (instance-or-prototype prc::persistent-object))
+  (when (dmm::authorize-operation 'dmm::write-entity-operation :-entity- class)
     (call-next-method)))
