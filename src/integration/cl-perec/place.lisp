c3322715f8ada2d11a1942e54e5df9bde17c6e5a
Levente Mészáros <levente.meszaros@gmail.com>
2008-12-09 11:13:33 +0000

    Kludge slot type

--- src/integration/cl-perec/place.lisp
+++ src/integration/cl-perec/place.lisp
@@ -5,7 +5,13 @@
 (in-package :hu.dwim.wui)
 
 (def method slot-type ((slot prc::persistent-slot-definition))
-  (prc::specified-type-of slot))
+  ;; TODO: KLUDGE: we cannot use canonical type because it does not allow dispatching on custom types (they are not in the canonical form)
+  ;; unfortunately we cannot use specified type either because it may contain and, or, not functionals
+  (bind ((specified-type (prc::specified-type-of slot)))
+    (if (and (consp specified-type)
+             (eq (first specified-type) 'and))
+        (prc::canonical-type-of slot)
+        specified-type)))
 
 (def method slot-value-place-editable-p ((place slot-value-place) (class dmm::entity) (instance prc::persistent-object) (slot dmm::effective-property))
   (dmm::editable-p slot))
