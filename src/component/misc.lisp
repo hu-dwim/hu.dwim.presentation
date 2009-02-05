c9fc8714d57d9142fe18c1476caec09be9c91bac
Attila Lendvai <attila@lendvai.name>
2009-02-05 14:03:30 +0000

    added render-tooltip

--- src/component/misc.lisp
+++ src/component/misc.lisp
@@ -268,6 +268,41 @@
   ()
   (:documentation "Abstract base class for components which show their component value in detail."))
 
+
+;;;;;;
+;;; tooltip
+
+;; OPTIMIZATION: this could collect the essential data in a special variable and at the end of rendering emit a literal js array with all the tooltips
+(def (function e) render-tooltip (connect-id tooltip &key position)
+  ":position might be '(\"below\" \"right\")"
+  (check-type tooltip (or string action function))
+  (check-type connect-id string)
+  (etypecase tooltip
+    (string
+     `js(on-load
+         (new dijit.Tooltip
+              (create :connectId (array ,connect-id)
+                      :label ,tooltip
+                      :position (array ,@position)))))
+    ((or action uri)
+     `js(on-load
+         (new dojox.widget.DynamicTooltip
+              (create :connectId (array ,connect-id)
+                      :position (array ,@position)
+                      :href ,(etypecase tooltip
+                               (action (register-action/href tooltip :delayed-content #t))
+                               (uri (print-uri-to-string tooltip)))))))
+    ;; action is subtypep function, therefore this order and the small code duplication...
+    (function
+     `js(on-load
+         (new dijit.Tooltip
+              (create :connectId (array ,connect-id)
+                      :label ,(babel:octets-to-string (with-output-to-sequence (*html-stream* :external-format (external-format-of *response*))
+                                                        (funcall tooltip))
+                                                      :encoding (external-format-of *response*))
+                      :position (array ,@position)))))))
+
+
 ;;;;;;
 ;;; Context sensitive help
 
