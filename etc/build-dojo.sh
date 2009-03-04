ecbe557cb89c25a2e473112228cc1f244b169666
Attila Lendvai <attila@lendvai.name>
2009-03-04 18:14:06 +0000

    smarter build-dojo.sh

--- etc/build-dojo.sh
+++ etc/build-dojo.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # http://docs.dojocampus.org/build/buildScript
 # example usage: ~/workspace/wui/etc/build-dojo.sh ~/workspace/ebr42/etc/ebr42-dojo-profile.js "en-us,hu"
@@ -19,20 +19,42 @@ absolutize ()
 LOCALE_LIST="en-us"
 WUI_HOME="`dirname $0`/.."
 DOJO_HOME="`dirname $0`/../../dojo"
+PROFILE="${WUI_HOME}/etc/wui/profile.js"
+
+TEMP=`getopt -o h --long help,dojo:,wui:,profile:,locales: -n "$0" -- "$@"`
+
+# echo $TEMP
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+while true ; do
+	case "$1" in
+        	-h|--help)	echo TODO usage...
+        		        exit 0
+        		        ;;
+        	--wui) WUI_HOME=$2 ; shift 2
+                     PROFILE="${WUI_HOME}/etc/wui/profile.js"
+        	     ;;
+        	--dojo) DOJO_HOME=$2 ; shift 2
+        	      ;;
+        	--locales) LOCALE_LIST=$2 ; shift 2
+        	         ;;
+        	--profile) PROFILE=$2 ; shift 2
+	                 ;;
+		--) shift ; break ;;
+		*) echo "Internal error at $1!" ; exit 1 ;;
+	esac
+done
+
+#echo "Remaining arguments:"
+#for arg do echo '--> '"\`$arg'" ; done
 
 WUI_HOME=`absolutize "$WUI_HOME"`
 DOJO_HOME=`absolutize "$DOJO_HOME"`
 RELEASE_DIR=${WUI_HOME}/wwwroot/
-
-PROFILE="${WUI_HOME}/etc/wui/profile.js"
-
-if [ "$1" != "" ]; then
-    PROFILE=$1
-fi
-
-if [ "$2" != "" ]; then
-    LOCALE_LIST=$2
-fi
 
 echo "Will build dojo into ${RELEASE_DIR} now..."
 echo "Assuming the following parameters:"
