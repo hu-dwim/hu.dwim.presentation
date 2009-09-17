676dd47ec50a6ce5aa5d37c87613b5a2e24e89fe
Levente Mészáros <levente.meszaros@gmail.com>
2009-09-17 14:06:10 +0000

    Project documentation

--- source/component/model/project.lisp
+++ source/component/model/project.lisp
@@ -20,7 +20,6 @@
 
 (def layered-method make-alternatives ((component project/inspector) class prototype value)
   (list* (delay-alternative-component-with-initargs 'project/detail/inspector :component-value value)
-         (delay-alternative-component-with-initargs 'project/content/inspector :component-value value)
          (call-next-method)))
 
 (def method localized-instance-name ((project project))
@@ -34,13 +33,33 @@
 
 (def layered-method refresh-component :before ((self project/detail/inspector))
   (bind (((:slots hu.dwim.wui::tab-pages component-value) self))
-    (setf hu.dwim.wui::tab-pages
-          (list (tab-page/widget (:selector "Description")
-                  "TODO")
-                (tab-page/widget (:selector "Content")
-                  (make-instance 'project/content/inspector :component-value component-value))
-                (tab-page/widget (:selector "Repository")
-                  (make-instance 'project/repository/inspector :component-value component-value))))))
+    (setf hu.dwim.wui::tab-pages (make-project-tab-pages self component-value))))
+
+(def (generic e) make-project-tab-pages (component project)
+  (:method ((component project/detail/inspector) (project project))
+    (list (tab-page/widget (:selector "Description")
+            (make-instance 'project/description/inspector :component-value project))
+          (tab-page/widget (:selector "Content")
+            (make-instance 'project/content/inspector :component-value project))
+          (tab-page/widget (:selector "Repository")
+            (make-instance 'project/repository/inspector :component-value project))
+          (tab-page/widget (:selector "Test")
+            (make-instance 'project/test/inspector :component-value project))
+          (tab-page/widget (:selector "Licence")
+            (make-instance 'project/licence/inspector :component-value project)))))
+
+;;;;;;
+;;; project/description/inspector
+
+(def (component e) project/description/inspector (inspector/style t/detail/presentation content/widget)
+  ())
+
+(def refresh-component project/description/inspector
+  (bind (((:slots content component-value) -self-)
+         (system (asdf:find-system (project-system-name component-value))))
+    (setf content (or (when (slot-boundp system 'asdf::description)
+                        (asdf:system-description system))
+                      "No description"))))
 
 ;;;;;;
 ;;; project/content/inspector
@@ -49,8 +68,7 @@
   ((directory :type component)))
 
 (def refresh-component project/content/inspector
-  (bind (((:slots directory) -self-)
-         (component-value (component-value-of -self-)))
+  (bind (((:slots directory component-value) -self-))
     (setf directory (make-value-inspector (path-of component-value) :initial-alternative-type 'pathname/directory/tree/inspector))))
 
 (def render-xhtml project/content/inspector
@@ -65,8 +83,31 @@
 
 (def render-xhtml project/repository/inspector
   ;; TODO:
-  (bind ((component-value (component-value-of -self-)))
+  (bind (((:slots component-value) -self-))
     (if (probe-file (merge-pathnames "_darcs" (path-of component-value)))
         <iframe (:width "100%" :height "600px" :style "border: none;"
-                 :src `str("cgi-bin/darcsweb.cgi?r=LIVE " ,(name-of component-value) ";a=summary"))>
+                 :src `str("/cgi-bin/darcsweb.cgi?r=LIVE " ,(name-of component-value) ";a=summary"))>
         <span "Unknown or no repository for " ,(name-of component-value)>)))
+
+;;;;;;
+;;; project/test/inspector
+
+(def (component e) project/test/inspector (inspector/style t/detail/presentation content/widget)
+  ())
+
+(def refresh-component project/test/inspector
+  (bind (((:slots content component-value) -self-)
+         (test-package-name (system-package-name (asdf:find-system (system-test-system-name (asdf:find-system (project-system-name component-value)))))))
+    (setf content (make-value-inspector (funcall (find-symbol "FIND-TEST" :hu.dwim.stefil) (find-symbol "TEST" test-package-name))
+                                        :initial-alternative-type 'test/hierarchy/tree/inspector))))
+
+;;;;;;
+;;; project/licence/inspector
+
+(def (component e) project/licence/inspector (inspector/style t/detail/presentation content/widget)
+  ())
+
+(def refresh-component project/licence/inspector
+  (bind (((:slots content component-value) -self-)
+         (licence-pathname (project-licence-pathname component-value)))
+    (setf content (make-value-inspector licence-pathname :initial-alternative-type 'pathname/text-file/inspector))))
--- source/util/project.lisp
+++ source/util/project.lisp
@@ -7,19 +7,10 @@
 (in-package :hu.dwim.wui)
 
 ;;;;;;
-;;; Projects
-
-(def special-variable *projects* (make-hash-table))
-
-(def (function e) find-project (name)
-  (gethash name *projects*))
-
-(def (function e) (setf find-project) (new-value name)
-  (setf (gethash name *projects*) new-value))
-
-
-;;;;;;
 ;;; Project
+
+(def (namespace e) project (&rest args &key &allow-other-keys)
+  `(make-instance 'project ,@args))
 
 (def (class* e) project ()
   ((name nil :type string)
@@ -30,3 +21,20 @@
     (unless name
       (setf name (or (pathname-name path)
                      (last-elt (pathname-directory path)))))))
+
+;;;;;;
+;;; Util
+
+(def (function e) find-project-by-path (pathname)
+  (maphash-values (lambda (project)
+                    (when (equal (path-of project) pathname)
+                      (return-from find-project-by-path project)))
+                  *projects*))
+
+(def (function e) project-system-name (project)
+  (name-of project))
+
+(def (function e) project-licence-pathname (project)
+  (bind ((licence-pathname (merge-pathnames "LICENCE" (path-of project))))
+    (when (probe-file licence-pathname)
+      licence-pathname)))
