9b44328219c9b3f878bd76613a8a02ecb7b8d646
Levente Mészáros <levente.meszaros@gmail.com>
2009-09-24 13:17:19 +0000

    Added text.lisp

---
+++ source/component/text/text.lisp
@@ -0,0 +1,27 @@
+;;; -*- mode: Lisp; Syntax: Common-Lisp; -*-
+;;;
+;;; Copyright (c) 2009 by the authors.
+;;;
+;;; See LICENCE for details.
+
+(in-package :hu.dwim.wui)
+
+;;;;;;
+;;; t/text/inspector
+
+(def (component e) t/text/inspector (inspector/style contents/widget)
+  ())
+
+(def refresh-component t/text/inspector
+  (bind (((:slots contents component-value) -self-))
+    (setf contents (mapcar [make-value-inspector !1 :initial-alternative-type 't/text/inspector]
+                           (contents-of component-value)))))
+
+(def render-text t/text/inspector
+  (iter (for content :in (contents-of -self-))
+        (write-text-line-begin)
+        (render-component content)
+        (write-text-line-separator)))
+
+(def method render-command-bar-for-alternative? ((component t/text/inspector))
+  #f)
