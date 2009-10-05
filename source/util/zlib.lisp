b56cd0b35e3d337c5f8870755a27a9c79784b7e7
Levente Mészáros <levente.meszaros@gmail.com>
2009-10-05 16:19:09 +0000

    Follow renames.

--- source/util/zlib.lisp
+++ source/util/zlib.lisp
@@ -338,7 +338,7 @@ Note that the size of the DESTINATION array should be at least 0.1% more than th
 
 (with-open-file (input "/tmp/x.diff" :direction :input :element-type '(unsigned-byte 8))
   (with-open-file (output "/tmp/x.diff.z" :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede)
-    (hu.dwim.zlib:deflate
+    (hu.dwim.wui.zlib:deflate
         (lambda (buffer start size)
           (read-sequence buffer input :start start :end size))
         (lambda (buffer start size)
@@ -346,7 +346,7 @@ Note that the size of the DESTINATION array should be at least 0.1% more than th
 
 (with-open-file (input "/tmp/x.diff.z" :direction :input :element-type '(unsigned-byte 8))
   (with-open-file (output "/tmp/x.diff.extracted" :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede)
-    (hu.dwim.zlib:inflate
+    (hu.dwim.wui.zlib:inflate
      (lambda (buffer start size)
        (read-sequence buffer input :start start :end size))
      (lambda (buffer start size)
