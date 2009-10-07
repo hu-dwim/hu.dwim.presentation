21fb1714376d3fb27033470b38f3d9c057a29a43
Levente Mészáros <levente.meszaros@gmail.com>
2009-10-07 22:19:46 +0000

    Rename number-of...

--- source/util/util.lisp
+++ source/util/util.lisp
@@ -373,27 +373,27 @@
 ;;;;;;
 ;;; Integer to string
 
-(def constant +number-of-integer-strings-to-cache+ 128)
+(def constant +integer-strings-to-cache-size+ 128)
 
 (def (constant :test 'equalp) +cached-integer-names+
-    (coerce (iter (for idx :from 0 :below +number-of-integer-strings-to-cache+)
+    (coerce (iter (for idx :from 0 :below +integer-strings-to-cache-size+)
                   (collect (coerce (princ-to-string idx) 'simple-base-string)))
-            `(simple-array string (,+number-of-integer-strings-to-cache+))))
+            `(simple-array string (,+integer-strings-to-cache-size+))))
 
 (def (function io) %integer-to-string (integer &key minimum-column-count (maximum-digit-count most-positive-fixnum) (divisor 10))
   (declare (type integer integer)
            (type (or null fixnum) minimum-column-count)
            (type fixnum maximum-digit-count))
-  (if (< integer +number-of-integer-strings-to-cache+)
+  (if (< integer +integer-strings-to-cache-size+)
       (aref +cached-integer-names+ integer)
       (bind ((remainder integer)
              (digit 0)
-             (number-of-digits 0)
+             (digits-count 0)
              (digits (list))
              (result-index 0)
              (result (make-array 128 :element-type 'base-char)))
         (declare (dynamic-extent digits result)
-                 (type fixnum digit number-of-digits result-index))
+                 (type fixnum digit digits-count result-index))
         (macrolet ((emit (char)
                      `(progn
                         (setf (aref result result-index) ,char)
@@ -401,10 +401,10 @@
           (iter (repeat maximum-digit-count)
                 (setf (values remainder digit) (truncate remainder divisor))
                 (push digit digits)
-                (incf number-of-digits)
+                (incf digits-count)
                 (until (zerop remainder)))
           (when minimum-column-count
-            (bind ((padding-length (- minimum-column-count number-of-digits)))
+            (bind ((padding-length (- minimum-column-count digits-count)))
               (when (plusp padding-length)
                 (iter (repeat padding-length)
                       (emit #\0)))))
