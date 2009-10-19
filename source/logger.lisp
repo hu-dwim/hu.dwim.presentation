28a5af54a861b483a7261732d4223284585494eb
Levente Mészáros <levente.meszaros@gmail.com>
2009-10-19 09:39:43 +0000

    Rely on default logger levels.

--- source/logger.lisp
+++ source/logger.lisp
@@ -6,20 +6,20 @@
 
 (in-package :hu.dwim.wui)
 
-(def logger wui ()
-  :runtime-level (if *load-as-production?* +info+ +debug+)
-  :compile-time-level (if *load-as-production?* +debug+ +dribble+)
-  :appender (make-instance 'brief-stream-appender :stream *debug-io*))
+(def logger wui () :appender (make-instance 'brief-stream-appender :stream *debug-io*))
 
 (def logger rerl (wui))
 
 (def logger timer (wui))
 
 (def logger http   (rerl))
+
 (def logger app    (rerl))
+
 (def logger l10n   (app))
 
 (def logger server (rerl))
+
 (def logger files  (server))
 
 (def logger threads (wui))
