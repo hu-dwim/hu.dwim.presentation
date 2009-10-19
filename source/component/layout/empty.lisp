ee34cd5500763fab6c9be9b993ec822d25d3aeb3
Levente Mészáros <levente.meszaros@gmail.com>
2009-10-19 16:01:38 +0000

    Split empty/layout into empty/layout and empty/layout/singleton.

--- source/component/layout/empty.lisp
+++ source/component/layout/empty.lisp
@@ -7,22 +7,42 @@
 (in-package :hu.dwim.wui)
 
 ;;;;;;
-;;; empty/layout
+;;; empty/abstract
 
 (eval-always
-  ;; NOTE: Do not include parent/mixin in the superclasses, because does not work with being a singleton.
-  ;;       The empty/layout supposed to be the substitute for NIL which is not a valid component.
-  (def (component e) empty/layout (layout/abstract)
+  (def (component e) empty/abstract (layout/abstract)
     ()
-    (:documentation "An EMPTY/LAYOUT is completely empty, it is practically INVISIBLE. For performance reasons it is used as a singleton, and it does not support PARENT-COMPONENT-OF. The reason to use EMPTY/LATOUT instead of NIL is to make NIL an invalid COMPONENT for debugging purposes. The component EMPTY/LAYOUT does not have any state.")))
+    (:documentation "An EMPTY/ABSTRACT is completely empty, it is practically INVISIBLE. The reason to use EMPTY/ABSTRACT instead of NIL, is to be able to make NIL an invalid COMPONENT for debugging purposes. The base component EMPTY/ABSTRACT does not have any state.")))
 
-(def load-time-constant +empty-layout-singleton-instance+ (make-instance 'empty/layout))
-
-(def (macro e) empty/layout ()
-  '+empty-layout-singleton-instance+)
-
-(def render-component empty/layout
+(def render-component empty/abstract
   (values))
 
+;;;;;;
+;;; empty/layout/singleton
+
+(eval-always
+  ;; NOTE: Do not include parent/mixin in the superclasses, because that does not work with being a singleton.
+  ;; The empty/layout/singleton is the substitute for NIL, because NIL is not a valid component.
+  ;; The number of effective slots in this class supposed to be zero.
+  (def (component e) empty/layout/singleton (empty/abstract)
+    ()
+    (:documentation "To save memory an EMPTY/LAYOUT/SINGLETON is used as a singleton, and it does not support PARENT-COMPONENT-OF.")))
+
+(def load-time-constant +empty-layout-singleton-instance+ (make-instance 'empty/layout/singleton))
+
+(def (macro e) empty/layout/singleton ()
+  '+empty-layout-singleton-instance+)
+
 (def (function e) empty-layout? (component)
-  (eq +empty-layout-singleton-instance+ component))
+  (or (eq +empty-layout-singleton-instance+ component)
+      (typep component 'empty/abstract)))
+
+;;;;;;
+;;; empty/layout
+
+(def (component e) empty/layout (empty/abstract parent/mixin)
+  ()
+  (:documentation "An EMPTY/LAYOUT may be used as part of the COMPONENT hierarchy, that is it supports PARENT-COMPONENT-OF."))
+
+(def (macro e) empty/layout ()
+  '(make-instance 'empty/layout))
