ba7a3425603968e6012cce03351a537d8a706e37
Levente Mészáros <levente.meszaros@gmail.com>
2009-11-02 11:50:32 +0000

    Revive.

--- source/component/widget/file-up-and-download.lisp
+++ source/component/widget/file-up-and-download.lisp
@@ -7,50 +7,54 @@
 (in-package :hu.dwim.wui)
 
 ;;;;;;
-;;; File download
+;;; Download file
 
-(def (component e) file-download-component (command/widget)
+(def (component e) download-file/widget (command/widget)
   ((directory "/tmp/")
    (file-name)
    (url-prefix "static/"))
-  (:default-initargs
-     :content (icon download)
-     :action nil))
+  (:default-initargs :content (icon download-file) :action nil))
 
-(def constructor (file-download-component (label nil label?) &allow-other-keys) ()
+(def (macro e) download-file/widget (&rest args &key &allow-other-keys)
+  `(make-instance 'download-file/widget ,@args))
+
+(def constructor (download-file/widget (label nil label?) &allow-other-keys) ()
   (when label?
-    (setf (content-of -self-) (icon download :label label))))
+    (setf (content-of -self-) (icon download-file :label label))))
 
-(def refresh-component file-download-component
+(def refresh-component download-file/widget
   (bind (((:slots file-name action url-prefix) -self-))
     (setf action (make-uri :path (string+ url-prefix (namestring file-name))))))
 
-(def render-xhtml file-download-component
+(def render-xhtml download-file/widget
   (bind ((absolute-file-name (merge-pathnames (file-name-of -self-) (directory-of -self-))))
     (unless (probe-file absolute-file-name)
       (setf (enabled-component? -self-) #f))
-    <div (:class "file-download")
+    <div (:class "download-file widget")
          ,(call-next-method)
          " (" ,(file-last-modification-timestamp absolute-file-name) ")">))
 
 ;;;;;;
-;;; File upload
+;;; Upload file
 
-(def (component e) file-upload-component (primitive-component)
+(def (component e) upload-file/widget ()
   ())
 
-(def render-xhtml file-upload-component
-  (ensure-client-state-sink -self-)
-  (render-file-upload-field :client-state-sink (client-state-sink-of -self-)))
+(def (macro e) upload-file/widget ()
+  '(make-instance 'upload-file/widget))
 
-(def method parse-component-value ((component file-upload-component) client-value)
+(def render-xhtml upload-file/widget
+  (ensure-client-state-sink -self-)
+  (render-upload-file-field :client-state-sink (client-state-sink-of -self-)))
+
+(def method parse-component-value ((component upload-file/widget) client-value)
   (etypecase client-value
-    (string (values nil #t)) ; empty file upload field is posted from the browser
+    (string (values nil #t)) ; empty upload file field is posted from the browser
     (rfc2388-binary:mime-part client-value)))
 
 ;;;;;;
 ;;; Icon
 
-(def (icon e) download)
+(def (icon e) download-file)
 
-(def (icon e) upload)
+(def (icon e) upload-file)
--- source/component/widget/wizard.lisp
+++ source/component/widget/wizard.lisp
@@ -9,49 +9,56 @@
 ;;;;;;
 ;;; Wizard
 
-(def (component e) wizard-component ()
+(def (component e) wizard/widget ()
   ((pages nil :type components)
    (page-navigation-bar nil :type component)))
 
-(def constructor wizard-component ()
+(def (macro e) wizard/widget ((&rest args &key &allow-other-keys) &body pages)
+  `(make-instance 'wizard/widget ,@args :pages (list ,@pages)))
+
+(def constructor wizard/widget ()
   (bind (((:slots content pages page-navigation-bar) -self-))
-    (setf page-navigation-bar (make-instance 'wizard-navigation-bar-component
+    (setf page-navigation-bar (make-instance 'wizard-navigation-bar/widget
                                              :position 0
                                              :total-count (length pages)
                                              :page-size 1))))
 
-(def render-xhtml wizard-component
+(def render-xhtml wizard/widget
   (bind (((:read-only-slots pages page-navigation-bar) -self-))
-    (render-vertical-list (list (elt pages (position-of page-navigation-bar)) page-navigation-bar))))
+    (render-vertical-list-layout (list (elt pages (position-of page-navigation-bar)) page-navigation-bar))))
 
-(def generic finish-wizard (wizard))
+(def layered-function finish-wizard (wizard))
 
-(def generic cancel-wizard (wizard))
+(def layered-function cancel-wizard (wizard))
 
 ;;;;;;
 ;;; Wizard navigation bar
 
-(def (component e) wizard-navigation-bar-component (page-navigation-bar-component)
+(def (component e) wizard-navigation-bar/widget (page-navigation-bar/widget)
   ((finish-command :type component)
    (cancel-command :type component)))
 
-(def constructor wizard-navigation-bar-component ()
+(def constructor wizard-navigation-bar/widget ()
   (bind (((:slots finish-command cancel-command) -self-))
     (setf finish-command (make-finish-wizard-command -self-)
           cancel-command (make-cancel-wizard-command -self-))))
 
-(def render-xhtml wizard-navigation-bar-component
+(def render-xhtml wizard-navigation-bar/widget
   (bind (((:read-only-slots first-command previous-command next-command last-command finish-command cancel-command) -self-))
     (render-horizontal-list-layout (list first-command previous-command next-command last-command finish-command cancel-command))))
 
 (def (function e) make-finish-wizard-command (wizard)
   (command/widget ()
-    (icon finish)
+    (icon finish-wizard)
     (make-component-action wizard
       (finish-wizard wizard))))
 
 (def (function e) make-cancel-wizard-command (wizard)
   (command/widget ()
-    (icon cancel)
+    (icon cancel-wizard)
     (make-component-action wizard
       (cancel-wizard wizard))))
+
+(def (icon e) finish-wizard)
+
+(def (icon e) cancel-wizard)
