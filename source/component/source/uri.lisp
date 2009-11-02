9a8d2786b4bb5045a0f1290be5b597738cfa1470
Levente Mészáros <levente.meszaros@gmail.com>
2009-11-02 12:14:43 +0000

    Add icon to external link widget.

--- source/component/source/uri.lisp
+++ source/component/source/uri.lisp
@@ -16,6 +16,7 @@
   'uri/inspector)
 
 (def layered-method make-alternatives ((component uri/inspector) class prototype (value uri))
+  ;; TOOD: not all uris are external links
   (list* (delay-alternative-component-with-initargs 'uri/external-link/inspector :component-value value)
          (call-next-method)))
 
@@ -28,7 +29,10 @@
 (def render-xhtml uri/external-link/inspector
   (with-render-style/abstract (-self- :element-name "span")
     (bind ((uri (print-uri-to-string (component-value-of -self-))))
-      <a (:href ,uri :target "_new") ,uri>)))
+      ;; TODO: refactor this to use the external-link/widget
+      <a (:href ,uri :target "_new")
+        ,uri
+        ,(render-component (icon external-link))>)))
 
 (def render-text uri/external-link/inspector
   (render-component (print-uri-to-string (component-value-of -self-))))
