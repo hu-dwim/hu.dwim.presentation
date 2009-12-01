c156eaee633e3d50da658baf6245651b7fc5a080
Attila Lendvai <attila@lendvai.name>
2009-12-01 20:00:04 +0000

    drop duplicate too-many-sessions definitions

--- source/server/conditions.lisp
+++ source/server/conditions.lisp
@@ -14,13 +14,3 @@
 
 (def function broker-recursion-limit-reached (&optional (brokers *brokers*))
   (error 'broker-recursion-limit-reached :request *request* :brokers brokers))
-
-(def condition* too-many-sessions (request-processing-error)
-  ((application nil))
-  (:report
-   (lambda (error stream)
-     (format stream "Too many active session for application ~S, the server seems to be overloaded."
-             (application-of error)))))
-
-(def function too-many-sessions (application)
-  (error 'too-many-sessions :request *request* :application application))
