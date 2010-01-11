adcbfa0f06d03624381b8d07eb56f72c5cd68798
Levente Mészáros <levente.meszaros@gmail.com>
2010-01-11 16:50:26 +0000

    Added two necessary meta-model:operation to perec/meta-model kludge.

--- integration/perec/kludge.lisp
+++ integration/perec/kludge.lisp
@@ -32,6 +32,12 @@
   (unless (eq value (class-prototype class))
     (hu.dwim.perec::load-instance value)))
 
+(def (hu.dwim.meta-model::operation e) export-instance-operation ()
+  ())
+
+(def (hu.dwim.meta-model::operation e) expand-instance-operation ()
+  ())
+
 ;; KLUDGE: TODO: redefined for now
 #+nil
 (def function update-component-value-from-place (place component)
