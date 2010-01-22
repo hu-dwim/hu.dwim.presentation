bc115eb6723570c5221c8ae8c811852400e08e8f
Attila Lendvai <attila@lendvai.name>
2010-01-22 11:21:01 +0000

    /test/login/ and /test/logout/ entry points for test-application

--- test/application.lisp
+++ test/application.lisp
@@ -21,6 +21,45 @@
       (emit-html-document ()
         <h3 ,(or name "The name query parameter is not specified!")>))))
 
+(def method authenticate ((application test-application) (session session-with-login-support) (login-data login-data/identifier-and-password))
+  (bind ((entry (assoc (identifier-of login-data) '(("test" "test123")
+                                                    ("joe" nil))
+                       :test 'equal)))
+    (if (and entry
+             (equal (second entry) (password-of login-data)))
+        (first entry)
+        ;; authentication failed
+        nil)))
+
+(def entry-point (*test-application* :path +login-entry-point-path+)
+  (with-entry-point-logic/login-with-identifier-and-password ()
+    (make-raw-functional-response ()
+      (emit-simple-html-document-http-response (:title "Login" :status +http-ok+)
+        (if *session*
+            <table ()
+              <tr <td "*session*"> <td ,(princ-to-string *session*) >>
+              <tr <td "(authenticate-return-value-of *session*)"> <td ,(princ-to-string (authenticate-return-value-of *session*)) >>>
+            <p "There's no *session*">)
+        (flet ((render-login-link (identifier password)
+                 <a (:href ,(bind ((uri (make-uri-for-current-application +login-entry-point-path+)))
+                              (setf (uri-query-parameter-value uri "identifier") identifier)
+                              (setf (uri-query-parameter-value uri "password") password)
+                              (setf (uri-query-parameter-value uri "user-action") "t")
+                              (print-uri-to-string uri)))
+                    "Login with " ,identifier "/" ,password >
+                 <br>))
+          (render-login-link "test" "test123")
+          (render-login-link "test" "wrong-password")
+          (render-login-link "joe" nil))
+        <a (:href ,(print-uri-to-string (make-uri-for-current-application "logout/")))
+           "Logout">))))
+
+(def entry-point (*test-application* :path "logout/")
+  (with-session-logic (:requires-valid-session #f)
+    (when *session*
+      (logout-current-session))
+    (make-redirect-response (make-uri-for-current-application +login-entry-point-path+))))
+
 (def function render-mime-part-details (mime-part)
   <p "Mime part headers:">
   <table
@@ -86,6 +125,8 @@
 
 ;;;;;;
 ;;; Session application
+
+;; TODO this example might be obsolete and/or out of date
 
 (def special-variable *session-application* (make-instance 'standard-application :path-prefix "/session/"))
 
