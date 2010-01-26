bc9cf7a45ff9e93fde7d7183884e4b7804ee302c
Levente Mészáros <levente.meszaros@gmail.com>
2010-01-26 21:39:48 +0000

    Ensure that after a refresh-component all computed-slots are up-to-date, so the component does not look to-be-refreshed

--- source/component/api/component.lisp
+++ source/component/api/component.lisp
@@ -402,6 +402,11 @@ such as MAKE-INSTANCE, MAKE-MAKER, MAKE-VIEWER, MAKE-EDITOR, MAKE-INSPECTOR, MAK
 (def layered-method refresh-component ((self component))
   (values))
 
+(def layered-method refresh-component :after ((self component))
+  (bind ((class (class-of self)))
+    (dolist (slot (computed-slots-of (class-of self)))
+      (slot-value-using-class class self slot))))
+
 (def method to-be-refreshed-component? ((self component))
   #f)
 
