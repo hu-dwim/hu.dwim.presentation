15b01ffd1a839834544dcd3a690a6ab6dfd5fc77
Levente Mészáros <levente.meszaros@gmail.com>
2010-01-28 14:40:12 +0000

    Avoid error coming from unbound component-value slots.

--- source/component/api/component.lisp
+++ source/component/api/component.lisp
@@ -405,8 +405,7 @@ such as MAKE-INSTANCE, MAKE-MAKER, MAKE-VIEWER, MAKE-EDITOR, MAKE-INSPECTOR, MAK
 (def layered-method refresh-component :after ((self component))
   (bind ((class (class-of self)))
     (dolist (slot (computed-slots-of (class-of self)))
-      (when (slot-boundp-using-class class self slot)
-        (slot-value-using-class class self slot)))))
+      (slot-value-using-class class self slot))))
 
 (def method to-be-refreshed-component? ((self component))
   #f)
