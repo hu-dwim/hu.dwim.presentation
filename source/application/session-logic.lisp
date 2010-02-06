2f5dc40921c84933efe60a03421b4c74dce0effa
Attila Lendvai <attila@lendvai.name>
2010-02-06 16:02:01 +0000

    fix that http header leaking error

--- source/application/session-logic.lisp
+++ source/application/session-logic.lisp
@@ -201,9 +201,9 @@
             (new-frame-href (print-uri-to-string (make-uri-for-new-frame)))
             (args (list refresh-href new-frame-href)))
        (app.debug "Default HANDLE-REQUEST-TO-INVALID-FRAME is sending a frame out of sync response")
-       (make-functional-html-response ((+header/status+ +http-not-acceptable+)
-                                       ('#.+disallow-response-caching-header-values+))
-         (apply-localization-function 'render-frame-out-of-sync-error args))))
+       (disallow-response-caching
+        (make-functional-html-response ((+header/status+ +http-not-acceptable+))
+          (apply-localization-function 'render-frame-out-of-sync-error args)))))
     ((not *delayed-content-request*)
      (app.debug "Default HANDLE-REQUEST-TO-INVALID-FRAME is sending a redirect response to ~A" application)
      (make-redirect-response-for-current-application))
