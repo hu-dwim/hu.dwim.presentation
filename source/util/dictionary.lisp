825bb2d96736a03e065907935b39cf146a1cacb3
Levente Mészáros <levente.meszaros@gmail.com>
2010-02-26 17:37:12 +0000

    Make dictionaries namespace based.

--- source/util/dictionary.lisp
+++ source/util/dictionary.lisp
@@ -9,37 +9,19 @@
 ;; TODO: move this stuff to hu.dwim.util?
 
 ;;;;;;
-;;; Dictionaries
-
-(def special-variable *dictionaries* (make-hash-table))
-
-(def (function e) find-dictionary (name)
-  (gethash name *dictionaries*))
-
-(def (function e) (setf find-dictionary) (new-value name)
-  (setf (gethash name *dictionaries*) new-value))
-
-;;;;;;
 ;;; Dictionary
+
+(def (namespace e) dictionary (args &body definition-names)
+  (bind ((quoted-definition-names (mapcar [list 'quote !1] definition-names)))
+    `(make-instance 'dictionary ,@args
+                    :name ',-name-
+                    :definition-names (list ,@quoted-definition-names))))
 
 (def class* dictionary ()
   ((name :type symbol)
    (definition-names :type list)
    (documentation nil :type string))
   (:documentation "A DICTIONARY is a list of definition names referring to the related definitions."))
-
-;;;;;;
-;;; Definer
-
-(def (definer e :available-flags "e") dictionary (name args &body definition-names)
-  (bind ((quoted-definition-names (mapcar [list 'quote !1] definition-names)))
-    `(progn
-       (setf (find-dictionary ',name) (make-instance 'dictionary ,@args
-                                                     :name ',name
-                                                     :definition-names (list ,@quoted-definition-names)))
-       ,@(when (getf -options- :export)
-                   `((eval-when (:compile-toplevel :load-toplevel :execute)
-                       (export ',name)))))))
 
 ;; TODO: move?
 (def method localized-instance-name ((dictionary dictionary))
