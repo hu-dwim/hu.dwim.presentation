ef719f824ad426655e4fd61f9d07b17a2cd71ea1
Attila Lendvai <attila@lendvai.name>
2010-03-16 10:12:06 +0000

    test: follow auth, untested

--- test/authentication.lisp
+++ test/authentication.lisp
@@ -12,27 +12,30 @@
 (def special-variable *authentication-application* (make-instance 'authentication-application :path-prefix "/authentication/"))
 
 (def entry-point (*authentication-application* :path +login-entry-point-path+)
-  (with-entry-point-logic/login-with-identifier-and-password ()
-    (make-raw-functional-response ()
-      (emit-simple-html-document-http-response (:title "Login" :status +http-ok+)
-        (if *session*
-            <table ()
-              <tr <td "*session*"> <td ,(princ-to-string *session*) >>
-              <tr <td "(authenticate-return-value-of *session*)"> <td ,(princ-to-string (authenticate-return-value-of *session*)) >>>
-            <p "There's no *session*">)
-        (flet ((render-login-link (identifier password)
-                 <a (:href ,(bind ((uri (make-uri-for-current-application +login-entry-point-path+)))
-                              (setf (uri-query-parameter-value uri "identifier") identifier)
-                              (setf (uri-query-parameter-value uri "password") password)
-                              (setf (uri-query-parameter-value uri "user-action") "t")
-                              (print-uri-to-string uri)))
-                    "Login with " ,identifier "/" ,password >
-                 <br>))
-          (render-login-link "test" "test123")
-          (render-login-link "test" "wrong-password")
-          (render-login-link "joe" nil))
-        <a (:href ,(print-uri-to-string (make-uri-for-current-application "logout/")))
-           "Logout">))))
+  (call-with-entry-point-logic/login-with-identifier-and-password
+    (named-lambda login-entry-point-response-factory
+        (&key login-data (user-action? #f) (authentication-happened? #f) &allow-other-keys)
+      (declare (ignore login-data user-action? authentication-happened?))
+      (make-raw-functional-response ()
+        (emit-simple-html-document-http-response (:title "Login" :status +http-ok+)
+          (if *session*
+              <table ()
+                <tr <td "*session*"> <td ,(princ-to-string *session*) >>
+                <tr <td "(authenticate-return-value-of *session*)"> <td ,(princ-to-string (authenticate-return-value-of *session*)) >>>
+              <p "There's no *session*">)
+          (flet ((render-login-link (identifier password)
+                   <a (:href ,(bind ((uri (make-uri-for-current-application +login-entry-point-path+)))
+                                    (setf (uri-query-parameter-value uri "identifier") identifier)
+                                    (setf (uri-query-parameter-value uri "password") password)
+                                    (setf (uri-query-parameter-value uri "user-action") "t")
+                                    (print-uri-to-string uri)))
+                      "Login with " ,identifier "/" ,password >
+                   <br>))
+            (render-login-link "test" "test123")
+            (render-login-link "test" "wrong-password")
+            (render-login-link "joe" nil))
+          <a (:href ,(print-uri-to-string (make-uri-for-current-application "logout/")))
+             "Logout">)))))
 
 (def entry-point (*authentication-application* :path "logout/")
   (with-session-logic (:requires-valid-session #f)
