1eb7a5d36d10bfeb25b406c1dee23b1c04e18b60
Attila Lendvai <attila@lendvai.name>
2010-03-17 12:01:31 +0000

    odt/odf export: factor out document header

--- source/component/mixin/exportable.lisp
+++ source/component/mixin/exportable.lisp
@@ -63,94 +63,66 @@
 ;;;;;;
 ;;; ODT format
 
+(def with-macro* with-xml-document-header/open-document-format (stream &key (encoding (guess-encoding-for-http-response)))
+  (emit-xml-prologue :encoding encoding :stream stream :version "1.0")
+  <office:document (xmlns:office "urn:oasis:names:tc:opendocument:xmlns:office:1.0"
+                    xmlns:style "urn:oasis:names:tc:opendocument:xmlns:style:1.0"
+                    xmlns:text "urn:oasis:names:tc:opendocument:xmlns:text:1.0"
+                    xmlns:table "urn:oasis:names:tc:opendocument:xmlns:table:1.0"
+                    xmlns:draw "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0"
+                    xmlns:fo "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"
+                    xmlns:xlink "http://www.w3.org/1999/xlink"
+                    xmlns:dc "http://purl.org/dc/elements/1.1/"
+                    xmlns:meta "urn:oasis:names:tc:opendocument:xmlns:meta:1.0"
+                    xmlns:number "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0"
+                    xmlns:presentation "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0"
+                    xmlns:svg "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0"
+                    xmlns:chart "urn:oasis:names:tc:opendocument:xmlns:chart:1.0"
+                    xmlns:dr3d "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0"
+                    xmlns:math "http://www.w3.org/1998/Math/MathML"
+                    xmlns:form "urn:oasis:names:tc:opendocument:xmlns:form:1.0"
+                    xmlns:script "urn:oasis:names:tc:opendocument:xmlns:script:1.0"
+                    xmlns:config "urn:oasis:names:tc:opendocument:xmlns:config:1.0"
+                    xmlns:ooo "http://openoffice.org/2004/office"
+                    xmlns:ooow "http://openoffice.org/2004/writer"
+                    xmlns:oooc "http://openoffice.org/2004/calc"
+                    xmlns:dom "http://www.w3.org/2001/xml-events"
+                    xmlns:xforms "http://www.w3.org/2002/xforms"
+                    xmlns:xsd "http://www.w3.org/2001/XMLSchema"
+                    xmlns:xsi "http://www.w3.org/2001/XMLSchema-instance"
+                    xmlns:rpt "http://openoffice.org/2005/report"
+                    xmlns:of "urn:oasis:names:tc:opendocument:xmlns:of:1.2"
+                    xmlns:rdfa "http://docs.oasis-open.org/opendocument/meta/rdfa#"
+                    xmlns:field "urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0"
+                    xmlns:formx "urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0"
+                    office:version "1.2"
+                    office:mimetype "application/vnd.oasis.opendocument.spreadsheet")
+    <office:meta
+      <meta:generator "http://dwim.hu/project/hu.dwim.wui">
+      <meta:creation-date ()
+        ,(local-time:format-rfc3339-timestring nil (local-time:now))>>
+    <office:body
+      ,(-with-macro/body-)>>)
+
 (def layered-method export-odt ((self exportable/abstract))
-  (with-output-to-export-stream (*xml-stream* :content-type +odt-mime-type+ :external-format :utf-8)
-    (emit-xml-prologue :encoding (guess-encoding-for-http-response) :stream *xml-stream* :version "1.0")
-    <office:document (xmlns:office "urn:oasis:names:tc:opendocument:xmlns:office:1.0"
-                      xmlns:style "urn:oasis:names:tc:opendocument:xmlns:style:1.0"
-                      xmlns:text "urn:oasis:names:tc:opendocument:xmlns:text:1.0"
-                      xmlns:table "urn:oasis:names:tc:opendocument:xmlns:table:1.0"
-                      xmlns:draw "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0"
-                      xmlns:fo "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"
-                      xmlns:xlink "http://www.w3.org/1999/xlink"
-                      xmlns:dc "http://purl.org/dc/elements/1.1/"
-                      xmlns:meta "urn:oasis:names:tc:opendocument:xmlns:meta:1.0"
-                      xmlns:number "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0"
-                      xmlns:presentation "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0"
-                      xmlns:svg "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0"
-                      xmlns:chart "urn:oasis:names:tc:opendocument:xmlns:chart:1.0"
-                      xmlns:dr3d "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0"
-                      xmlns:math "http://www.w3.org/1998/Math/MathML"
-                      xmlns:form "urn:oasis:names:tc:opendocument:xmlns:form:1.0"
-                      xmlns:script "urn:oasis:names:tc:opendocument:xmlns:script:1.0"
-                      xmlns:config "urn:oasis:names:tc:opendocument:xmlns:config:1.0"
-                      xmlns:ooo "http://openoffice.org/2004/office"
-                      xmlns:ooow "http://openoffice.org/2004/writer"
-                      xmlns:oooc "http://openoffice.org/2004/calc"
-                      xmlns:dom "http://www.w3.org/2001/xml-events"
-                      xmlns:xforms "http://www.w3.org/2002/xforms"
-                      xmlns:xsd "http://www.w3.org/2001/XMLSchema"
-                      xmlns:xsi "http://www.w3.org/2001/XMLSchema-instance"
-                      xmlns:rpt "http://openoffice.org/2005/report"
-                      xmlns:of "urn:oasis:names:tc:opendocument:xmlns:of:1.2"
-                      xmlns:rdfa "http://docs.oasis-open.org/opendocument/meta/rdfa#"
-                      xmlns:field "urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0"
-                      xmlns:formx "urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0"
-                      office:version "1.2"
-                      office:mimetype "application/vnd.oasis.opendocument.spreadsheet")
-      <office:meta
-        <meta:generator "http://dwim.hu/project/hu.dwim.wui">
-        <meta:creation-date ()
-          ,(local-time:format-rfc3339-timestring nil (local-time:now))>>
-      <office:body
-        ,(render-odt self)>>))
+  (bind ((encoding (guess-encoding-for-http-response)))
+    (with-output-to-export-stream (*xml-stream* :content-type +odt-mime-type+ :external-format encoding)
+      (with-xml-document-header/open-document-format (*xml-stream* :encoding encoding))
+      <office:text
+        ,(render-odt self)>)))
 
 ;;;;;;
 ;;; ODS format
 
 (def layered-method export-ods ((self exportable/abstract))
-  (with-output-to-export-stream (*xml-stream* :content-type +ods-mime-type+ :external-format :utf-8)
-    (emit-xml-prologue :encoding (guess-encoding-for-http-response) :stream *xml-stream* :version "1.1")
-    <office:document (xmlns:office "urn:oasis:names:tc:opendocument:xmlns:office:1.0"
-                      xmlns:style "urn:oasis:names:tc:opendocument:xmlns:style:1.0"
-                      xmlns:text "urn:oasis:names:tc:opendocument:xmlns:text:1.0"
-                      xmlns:table "urn:oasis:names:tc:opendocument:xmlns:table:1.0"
-                      xmlns:draw "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0"
-                      xmlns:fo "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"
-                      xmlns:xlink "http://www.w3.org/1999/xlink"
-                      xmlns:dc "http://purl.org/dc/elements/1.1/"
-                      xmlns:meta "urn:oasis:names:tc:opendocument:xmlns:meta:1.0"
-                      xmlns:number "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0"
-                      xmlns:presentation "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0"
-                      xmlns:svg "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0"
-                      xmlns:chart "urn:oasis:names:tc:opendocument:xmlns:chart:1.0"
-                      xmlns:dr3d "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0"
-                      xmlns:math "http://www.w3.org/1998/Math/MathML"
-                      xmlns:form "urn:oasis:names:tc:opendocument:xmlns:form:1.0"
-                      xmlns:script "urn:oasis:names:tc:opendocument:xmlns:script:1.0"
-                      xmlns:config "urn:oasis:names:tc:opendocument:xmlns:config:1.0"
-                      xmlns:ooo "http://openoffice.org/2004/office"
-                      xmlns:ooow "http://openoffice.org/2004/writer"
-                      xmlns:oooc "http://openoffice.org/2004/calc"
-                      xmlns:dom "http://www.w3.org/2001/xml-events"
-                      xmlns:xforms "http://www.w3.org/2002/xforms"
-                      xmlns:xsd "http://www.w3.org/2001/XMLSchema"
-                      xmlns:xsi "http://www.w3.org/2001/XMLSchema-instance"
-                      xmlns:rpt "http://openoffice.org/2005/report"
-                      xmlns:of "urn:oasis:names:tc:opendocument:xmlns:of:1.2"
-                      xmlns:rdfa "http://docs.oasis-open.org/opendocument/meta/rdfa#"
-                      xmlns:field "urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0"
-                      xmlns:formx "urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0"
-                      office:version "1.2"
-                      office:mimetype "application/vnd.oasis.opendocument.spreadsheet")
-      <office:meta
-        <meta:generator "http://dwim.hu/project/hu.dwim.wui">
-        <meta:creation-date ()
-          ,(local-time:format-rfc3339-timestring nil (local-time:now))>>
-      <office:body
-        <office:spreadsheet
-         <table:table
-             ,(render-ods self)>>>>))
+  (bind ((encoding (guess-encoding-for-http-response)))
+    (with-output-to-export-stream (*xml-stream* :content-type +ods-mime-type+ :external-format encoding)
+      (with-xml-document-header/open-document-format (*xml-stream* :encoding encoding))
+      <office:spreadsheet
+       ;; TODO i think this table:table should be deleted. check if there can be multiple instances of them and what it means.
+       ;; based on that decide where to render it...
+       <table:table
+         ,(render-ods self)>>)))
 
 ;;;;;;
 ;;; SH format
