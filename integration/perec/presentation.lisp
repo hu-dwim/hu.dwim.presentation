4f360e2c41105f3ead6fbff7df46e18d4a6b55e3
Attila Lendvai <attila@lendvai.name>
2010-03-18 07:15:45 +0000

    housekeeping

--- integration/perec/presentation.lisp
+++ integration/perec/presentation.lisp
@@ -7,4 +7,8 @@
 (in-package :hu.dwim.wui)
 
 (def layered-method collect-slot-value-list/slots :around (component (class hu.dwim.perec::persistent-class) (prototype hu.dwim.perec::persistent-object) value)
-  (remove-slots '(hu.dwim.perec::oid hu.dwim.perec::persistent hu.dwim.perec::transaction hu.dwim.perec::transaction-event) (call-next-method)))
+  (remove-slots '(hu.dwim.perec::oid
+                  hu.dwim.perec::persistent
+                  hu.dwim.perec::transaction
+                  hu.dwim.perec::transaction-event)
+                (call-next-method)))
