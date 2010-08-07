3ef1898cf25736df716accffe09cbb18f5164b69
Attila Lendvai <attila@lendvai.name>
2010-08-07 11:38:29 +0000

    follow util temp files

--- source/http/request-parsing.lisp
+++ source/http/request-parsing.lisp
@@ -164,7 +164,7 @@
       (cond
         ((or (string-equal "file" content-disposition)
              (not (null filename)))
-         (bind (((:values file file-name) (open-temporary-file :name-prefix "wui-upload-")))
+         (bind (((:values file file-name) (open-temporary-file :file-name-prefix "wui-upload-")))
            (flet ((close-and-delete-temporary-file ()
                     (close file)
                     (delete-file file-name)))
