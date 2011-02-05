d0aaf851429df0fcf8ebab084ca78f8118617910
Attila Lendvai <attila@lendvai.name>
2011-02-05 02:46:10 +0000

    fix inheritance of component-rendering-response

--- source/component/api/response.lisp
+++ source/component/api/response.lisp
@@ -27,7 +27,7 @@
 ;;;;;;
 ;;; Component rendering response
 
-(def class* component-rendering-response (response)
+(def class* component-rendering-response (http-response)
   ((unique-counter 0 :type integer)
    (application :type application)
    (session :type session)
